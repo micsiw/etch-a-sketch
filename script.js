7592293b99eda1ce59e5f3340c2e4e37999c1741
siwardson <michalsiwanowicz@gmail.com>
2022-05-26 22:12:00 +0200

    added working eventlistener - mouse hover makes cells go black color

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const gridSquare = document.querySelector(".grid-square");
+const gridSquare = document.querySelector('.grid-square');
 
-for (c = 0; c < 16*16; c++) {
+for (c = 0; c < 30*30; c++) {
     let cell = document.createElement('div');
-    gridSquare.appendChild(cell).className = "cell";
+    gridSquare.appendChild(cell).className = 'cell';
 }
+
+const cells = document.querySelectorAll('.cell');
+
+cells.forEach((cell) => {
+    cell.addEventListener('mouseover', () => {
+    cell.style.backgroundColor = 'black'
+    });
+});
