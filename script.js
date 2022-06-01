9ed94d7a2854c7bcd11c58f2d8fd866c711c8474
siwardson <michalsiwanowicz@gmail.com>
2022-06-01 18:07:13 +0200

    trying out new way of displaying borders + rearrange js code to accept client values for grid size

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-const gridSquare = document.querySelector('.grid-square');
+let squaresPerSide = 64;
 
-for (c = 0; c < 30*30; c++) {
+const gridSquare = document.querySelector('.grid-square');
+gridSquare.style.gridTemplateColumns = "repeat(" + squaresPerSide + ", 1fr)"
+
+for (c = 0; c < Math.pow(squaresPerSide, 2); c++) {
     let cell = document.createElement('div');
     gridSquare.appendChild(cell).className = 'cell';
 }
 
 const cells = document.querySelectorAll('.cell');
 
-cells.forEach((cell) => {
+/*cells.forEach((cell) => {
     cell.addEventListener('mouseover', () => {
     cell.style.backgroundColor = 'black'
     });
-});
+
+});*/
