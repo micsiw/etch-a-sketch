be7f7439799aa1358e31244544a53ea049516c2e
siwardson <michalsiwanowicz@gmail.com>
2022-06-01 23:15:31 +0200

    added working slider to adjust grid size up to 80 squares

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
-let squaresPerSide = 64;
-
 const gridSquare = document.querySelector('.grid-square');
-gridSquare.style.gridTemplateColumns = "repeat(" + squaresPerSide + ", auto)";
+const slider = document.querySelector('.slider');
+const gridSizeInfo = document.querySelector('.grid-size-info');
 
+let squaresPerSide = slider.value;
 
-
-for (c = 0; c < Math.pow(squaresPerSide, 2); c++) {
-    let cell = document.createElement('div');
-    gridSquare.appendChild(cell).className = 'cell';
+slider.oninput = function() {
+    resetGrid();
+    squaresPerSide = slider.value;
+    drawGrid(squaresPerSide);
 }
 
-const cells = document.querySelectorAll('.cell');
+function drawGrid(squaresPerSide) {
 
-/*cells.forEach((cell) => {
-    cell.addEventListener('mouseover', () => {
-    cell.style.backgroundColor = 'black'
+    for (c = 0; c < Math.pow(squaresPerSide, 2); c++) {
+        let cell = document.createElement('div');
+        gridSquare.appendChild(cell).className = 'cell';
+    }
+
+    gridSquare.style.gridTemplateColumns = "repeat(" + squaresPerSide + ", auto)";
+    gridSizeInfo.innerHTML = squaresPerSide + " x " + squaresPerSide;
+
+    const cells = document.querySelectorAll('.cell');
+
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseover', () => {
+        cell.style.backgroundColor = 'black'
+        });
     });
+}
 
-});*/
+function resetGrid() {
+    const cells = document.querySelectorAll('.cell');
+    
+    if (cells.length > 0) {
+
+        for(var i = 0; i < cells.length; i++){
+            cells[i].parentNode.removeChild(cells[i]);
+        }
+    }
+}
+
+drawGrid(squaresPerSide);
+
+
+
