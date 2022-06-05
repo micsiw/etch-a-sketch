b46b5df73193f10c8718f7bdb6f7437fc0e29305
siwardson <michalsiwanowicz@gmail.com>
2022-06-05 16:40:46 +0200

    added erase button + toggle option for every mode

--- script.js
+++ script.js
@@ -8,9 +8,11 @@ const eraseButton = document.querySelector('.button-erase');
 let squaresPerSide = slider.value;
 
 slider.oninput = function() {
+
     resetGrid();
     squaresPerSide = slider.value;
     drawGrid(squaresPerSide);
+
 }
 
 function drawGrid(squaresPerSide) {
@@ -26,18 +28,22 @@ function drawGrid(squaresPerSide) {
     const cells = document.querySelectorAll('.cell');
 
     cells.forEach((cell) => {
-        cell.addEventListener('mouseover', hoverDraw)
+        cell.addEventListener('mouseover', hoverDraw);
         cell.style.filter = 'brightness(100%)';
     });
+
 }
 
 function hoverDraw() {
-    
-    this.style.backgroundColor = '#A0A0A0'
+
+    this.style.filter = 'brightness(100%)';
+    this.style.backgroundColor = '#A0A0A0';
 
 }
 
 function hoverRainbow() {
+
+    this.style.filter = 'brightness(100%)';
 
     function getColor() {
         return "hsla(" + ~~(360 * Math.random()) + "," +
@@ -53,61 +59,131 @@ function hoverShade() {
 
     switch(this.style.filter) {
         case 'brightness(100%)':
+            this.style.filter = 'brightness(90%)';
+            break;
+        case 'brightness(90%)':
             this.style.filter = 'brightness(80%)';
             break;
         case 'brightness(80%)':
+            this.style.filter = 'brightness(70%)';
+            break;  
+        case 'brightness(70%)':
             this.style.filter = 'brightness(60%)';
             break;
         case 'brightness(60%)':
+            this.style.filter = 'brightness(50%)';
+            break;
+        case 'brightness(50%)':
             this.style.filter = 'brightness(40%)';
-            break;  
+            break;
         case 'brightness(40%)':
             this.style.filter = 'brightness(30%)';
+            break;
+        case 'brightness(30%)':
+            this.style.filter = 'brightness(20%)';
             break;   
     }
-   
+
+}
+
+function hoverErase() {
+
+    this.style.filter = 'brightness(100%)';
+    this.style.backgroundColor = 'white';
+
 }
     
-
 function resetGrid() {
 
     const cells = document.querySelectorAll('.cell');
     
     if (cells.length > 0) {
 
-        for(var i = 0; i < cells.length; i++){
+        for(i = 0; i < cells.length; i++){
             cells[i].parentNode.removeChild(cells[i]);
         }
     }
+
+}
+
+function removeListeners() {
+
+    const cells = document.querySelectorAll('.cell');
+
+    cells.forEach((cell) => {
+        cell.removeEventListener('mouseover', hoverDraw);
+    });
+
+    cells.forEach((cell) => {
+        cell.removeEventListener('mouseover', hoverRainbow);
+    });
+
+    cells.forEach((cell) => {
+        cell.removeEventListener('mouseover', hoverShade);
+    });
+
 }
 
 rainbowButton.addEventListener('click', () => {
 
     const cells = document.querySelectorAll('.cell');
 
-    cells.forEach((cell) => {
-        cell.removeEventListener('mouseover', hoverDraw)
-    });
+    removeListeners();
 
     cells.forEach((cell) => {
-        cell.addEventListener('mouseover', hoverRainbow)
-
+        if (cell.classList.contains('rainbow')) {
+            cell.className = 'cell';
+            cell.addEventListener('mouseover', hoverDraw);
+        } else {
+            cell.className += ' rainbow'; 
+            /* added class rainbow to recognize if it's active, 
+            and toggle it with the same button */
+            cell.addEventListener('mouseover', hoverRainbow);
+        }
     });
+
 });
 
 shadeButton.addEventListener('click', () => {
 
     const cells = document.querySelectorAll('.cell');
 
+    removeListeners();
+
     cells.forEach((cell) => {
-        cell.removeEventListener('mouseover', hoverDraw)
+        if (cell.classList.contains('shade')) {
+            cell.className = 'cell';
+            cell.addEventListener('mouseover', hoverDraw);
+        } else {
+            cell.className += ' shade';
+            /* added class shade to recognize if it's active, 
+            and toggle it with the same button */
+            cell.addEventListener('mouseover', hoverShade);
+        }
+
     });
 
-    cells.forEach((cell) => {
-        cell.addEventListener('mouseover', hoverShade)
+});
 
+eraseButton.addEventListener('click', () => {
+
+    const cells = document.querySelectorAll('.cell');
+
+    removeListeners();
+
+    cells.forEach((cell) => {
+        if (cell.classList.contains('reset')) {
+            cell.className = 'cell';
+            cell.addEventListener('mouseover', hoverDraw);
+        } else {
+        cell.className += ' reset';
+        /* added class reset to recognize if it's active, 
+            and toggle it with the same button */
+        cell.addEventListener('mouseover', hoverErase);
+        }
     });
 });
+
 
 drawGrid(squaresPerSide);
 
